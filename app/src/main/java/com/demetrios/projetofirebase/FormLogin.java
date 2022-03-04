d7b93ac4a64969ba28b5256dcf59be474e7028a7
DemetriosP <pantaleao15@hotmail.com>
2022-03-03 22:52:06 -0300

    Implementação parcial da tela de login

---
+++ app/src/main/java/com/demetrios/projetofirebase/FormLogin.java
@@ -0,0 +1,15 @@
+package com.demetrios.projetofirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+    }
+
+}
