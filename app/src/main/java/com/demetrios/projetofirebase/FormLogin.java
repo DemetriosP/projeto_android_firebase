38fb04544ad8de2300acca786c5b7eacd0591d63
DemetriosP <pantaleao15@hotmail.com>
2022-03-04 14:28:29 -0300

    Implementação da tela de cadastro

--- app/src/main/java/com/demetrios/projetofirebase/FormLogin.java
+++ app/src/main/java/com/demetrios/projetofirebase/FormLogin.java
@@ -1,16 +1,34 @@
 package com.demetrios.projetofirebase;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Objects;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
-        getSupportActionBar().hide();
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        iniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(view -> {
+            Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+            startActivity(intent);
+        });
+    }
+
+    private void iniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+
     }
 
 }
