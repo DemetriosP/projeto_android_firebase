576ae783947aa25acde00875a2c5bde9ea4a7973
DemetriosP <pantaleao15@hotmail.com>
2022-03-04 15:58:37 -0300

    Implementação da tela de usuário

--- app/src/main/java/com/demetrios/projetofirebase/TelaPrincipal.java
+++ app/src/main/java/com/demetrios/projetofirebase/TelaPrincipal.java
@@ -3,17 +3,12 @@ package com.demetrios.projetofirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-import java.util.Objects;
-
-public class FormCadastro extends AppCompatActivity {
+public class TelaPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_cadastro);
-        Objects.requireNonNull(getSupportActionBar()).hide();
-
+        setContentView(R.layout.activity_tela_principal);
     }
 }
