916dd6d46b842520dbde77fa4cf2a9498929d9fb
DemetriosP <pantaleao15@hotmail.com>
2022-03-06 15:50:34 -0300

    Implementação do login através do firebase

--- app/src/main/java/com/demetrios/projetofirebase/FormLogin.java
+++ app/src/main/java/com/demetrios/projetofirebase/FormLogin.java
@@ -1,16 +1,30 @@
 package com.demetrios.projetofirebase;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
 public class FormLogin extends AppCompatActivity {
 
     private TextView text_tela_cadastro;
+    private EditText edit_email, edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressBar;
+    final String[] mensagens = {"Preencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +38,73 @@ public class FormLogin extends AppCompatActivity {
             Intent intent = new Intent(FormLogin.this, FormCadastro.class);
             startActivity(intent);
         });
+
+        bt_entrar.setOnClickListener(view -> {
+
+            String email = edit_email.getText().toString();
+            String senha = edit_senha.getText().toString();
+
+            if(email.isEmpty() || senha.isEmpty()){
+                Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_LONG);
+                snackbar.setBackgroundTint(Color.WHITE);
+                snackbar.setTextColor(Color.BLACK);
+                snackbar.show();
+            }else {
+                AutenticarUsuario(view);
+            }
+
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(usuarioAtual != null){
+            TelaPrincipal();
+        }
+    }
+
+    private void AutenticarUsuario(View view) {
+
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(task -> {
+
+            if(task.isSuccessful()){
+                progressBar.setVisibility(View.VISIBLE);
+                new Handler().postDelayed(this::TelaPrincipal, 3000);
+            }else{
+                String erro;
+                try {
+                    throw Objects.requireNonNull(task.getException());
+                }catch (Exception e){
+                    erro = "Erro ao logar usuário";
+                    Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_LONG);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+
+    }
+
+    private void TelaPrincipal(){
+        Intent intent = new Intent(FormLogin.this, TelaPrincipal.class);
+        startActivity(intent);
+        finish();
     }
 
     private void iniciarComponentes(){
         text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
-
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_cadastrar);
+        progressBar = findViewById(R.id.progress_bar);
     }
 
 }
