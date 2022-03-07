0cf95842d837096618bc82576d457f3ebf62568b
DemetriosP <pantaleao15@hotmail.com>
2022-03-06 21:12:46 -0300

    Implementação do Firebase na TelaPrincial

--- app/src/main/java/com/demetrios/projetofirebase/TelaPrincipal.java
+++ app/src/main/java/com/demetrios/projetofirebase/TelaPrincipal.java
@@ -1,14 +1,66 @@
 package com.demetrios.projetofirebase;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.Objects;
 
 public class TelaPrincipal extends AppCompatActivity {
+
+    private TextView nomeUsuario, emailUsuario;
+    private Button btDeslogar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        iniciarComponentes();
+
+        btDeslogar.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(TelaPrincipal.this, FormLogin.class);
+            startActivity(intent);
+            finish();
+        });
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String email = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail();
+
+        usuarioID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        DocumentReference referencia = db.collection("Usuarios").document(usuarioID);
+        referencia.addSnapshotListener((documentSnapshot, error) -> {
+            if(documentSnapshot != null){
+                nomeUsuario.setText(documentSnapshot.getString("nome"));
+                emailUsuario.setText(email);
+            }
+        });
+    }
+
+    private void iniciarComponentes() {
+        nomeUsuario = findViewById(R.id.text_nome_usuario);
+        emailUsuario = findViewById(R.id.text_email_usuario);
+        btDeslogar = findViewById(R.id.bt_deslogar);
+    }
+
 }
